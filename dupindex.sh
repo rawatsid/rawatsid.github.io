fa7f41ada4cd24af5c6d66ae9232b5f8ec0666fe
Sougata-Dhar <64148598+Sougata-Dhar@users.noreply.github.com>
2020-04-24 20:07:46 -0400

    Update dupindex.sh

--- dupindex.sh
+++ dupindex.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cp index.html research.html
 cp index.html publication.html
 cp index.html teaching.html
-cp index.html mgpusim.html
-cp index.html software.html
-cp index.html blog.html
+cp index.html presentation.html
+cp index.html contact.html
+
